94926f9ffe2a692ef683e388f76515e4c7f0530b
gkonars <Gabriel.KonarSteenberg@nrel.gov>
2025-04-08 17:24:43 -0600

    First attempt at learning a predefined policy

--- src/nsai_experiments/zoning_game/notebook_utils.py
+++ src/nsai_experiments/zoning_game/notebook_utils.py
@@ -5,6 +5,7 @@ def create_dataset_1_one_game(policy_creator, policy_seed, env_seed):
     env = ZoningGameEnv()
     policy = policy_creator(seed=policy_seed)
     states = []
+    moves = []
     values = []
     obs, info = env.reset(seed=env_seed)
     logger = logging.getLogger()
@@ -15,8 +16,9 @@ def create_dataset_1_one_game(policy_creator, policy_seed, env_seed):
             next_move = policy(obs)
             states.append(flatten_zg_obs(obs))
             values.append(env._eval_tile_grid_score())
+            moves.append(next_move)
             obs, reward, terminated, truncated, info = env.step(next_move, on_invalid=None)
             if terminated or truncated:
-                return states, values
+                return states, values, moves
     finally:
         logger.setLevel(previous_level)
